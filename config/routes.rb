3768bf16b04971033d586c6d697ed2e545ccc6bf
Hiro.Fujita <tengtianbilu@gmail.com>
2021-03-07 11:16:39 +0000

    huann

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,13 @@
 Rails.application.routes.draw do
-  root to: 'toppages#index'
+  root to: 'tasks#index'
   
   get 'signup', to: 'users#new'
   post 'login', to: 'sessions#create'
+  get 'login', to: 'sessions#new'
   delete 'logout', to: 'sessions#destroy'
   
   get 'signup', to: 'users#new'
   resources :users, only: [:index, :show, :create]
+  
+  resources :tasks, only: [:create, :destroy]
 end
