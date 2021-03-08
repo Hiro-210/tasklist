1df1077369f3b3dec5e06437f7b58d61cdfe5aa6
Hiro.Fujita <tengtianbilu@gmail.com>
2021-03-08 01:13:30 +0000

    wakaranai

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -23,7 +23,7 @@ class TasksController < ApplicationController
   private
 
   def task_params
-    params.require(:task).permit(:content, :title,)
+    params.require(:task).permit(:content, :title, :user_id)
   end
   
   def correct_user
--- app/models/task.rb
+++ app/models/task.rb
@@ -3,4 +3,5 @@ class Task < ApplicationRecord
     
     validates :content, presence: true, length: { maximum: 255 }
     validates :title, presence: true, length: { maximum: 255 }
+    validates :user_id, presence: true, length: { maximum: 255 }
 end
